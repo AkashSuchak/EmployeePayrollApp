4f2c665e660d3cfb1bf38781bc5b6a8cde7754d1
AkashSuchak <72850833+AkashSuchak@users.noreply.github.com>
2021-09-26 22:52:11 +0530

    feat:Added: Update Employee

--- js/utilities.js
+++ js/utilities.js
@@ -4,3 +4,10 @@ const stringifyDate = (date) => {
         new Date(date).toLocaleDateString('en-GB', options)
     return newDate
 }
+
+const update = (node) => {
+    let empPayrollData = empPayrollList.find(empData => empData._name == node.name);
+    if(!empPayrollData) return;
+    localStorage.setItem("editEmp", JSON.stringify(empPayrollData));
+    window.location.replace(site_properties.add_emp_payroll_page)
+}
